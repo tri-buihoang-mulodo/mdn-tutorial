9b6aadcef09e527e94b193921071bcd897f6f32e
Tri.buihoang.mulodo <tri.buihoang.mulodo@gmail.com>
2017-08-28 18:04:58 +0700

    Update HTML file #ref 29922

--- getting_started/scripts/main.js
+++ getting_started/scripts/main.js
@@ -2,7 +2,8 @@ var textarea = document.getElementById("code");
 var reset = document.getElementById("reset");
 var code = textarea.value;
 var output = document.querySelector(".output");
-var solution = document.getElementById("solution");
+var solution1 = document.getElementById("solution1");
+var solution2 = document.getElementById("solution2");
 
 function drawOutput() {
   output.innerHTML = textarea.value;
@@ -13,8 +14,12 @@ reset.addEventListener("click", function() {
   drawOutput();
 });
 
-solution.addEventListener("click", function() {
+solution1.addEventListener("click", function() {
   textarea.value = '<em>This is my text.</em>';
+  drawOutput();
+});
+solution2.addEventListener("click", function() {
+  textarea.value = '<p>A link to my <a href="https://www.mozilla.org/" title="The Mozilla homepage" target="_blank">favourite website</a>.</p>';
   drawOutput();
 });
 
