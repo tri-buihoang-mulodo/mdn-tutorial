709bbc67d9b9f040b754f3a3fa072ff43dcc7d34
BuiHoangTri <hoangtri.45@gmail.com>
2017-08-28 21:53:25 +0700

    Update HTML file #ref 29922

--- getting_started/scripts/main.js
+++ getting_started/scripts/main.js
@@ -22,6 +22,13 @@ solution2.addEventListener("click", function() {
   textarea.value = '<p>A link to my <a href="https://www.mozilla.org/" title="The Mozilla homepage" target="_blank">favourite website</a>.</p>';
   drawOutput();
 });
+solution3.addEventListener("click", function() {
+  textarea.value = '<p>I really enjoy <strong>playing the drums</strong>. One of my favourite drummers is Neal Peart, who\
+ plays in the band <a href="https://en.wikipedia.org/wiki/Rush_%28band%29" title="Rush Wikipedia article">Rush</a>.\
+ My favourite Rush album is currently <a href="http://www.deezer.com/album/942295">Moving Pictures</a>.</p>\
+<img src="http://www.cygnus-x1.net/links/rush/images/albums/sectors/sector2-movingpictures-cover-s.jpg">';
+  drawOutput();
+});
 
 textarea.addEventListener("input", drawOutput);
 window.addEventListener("load", drawOutput);
